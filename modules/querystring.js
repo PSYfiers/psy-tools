1ff4afcb6521d947dd1ee19d544e095c1397d038
PSYfiers AG <armin.neische@psyfiers.ch>
2023-11-03 15:04:04 +0100

    Removing console outputs

--- modules/querystring.js
+++ modules/querystring.js
@@ -12,25 +12,21 @@ module.exports = {
         return querystring.startsWith("&") ? querystring.substring(1):querystring
     },
     _encode(key, value) {
-        console.log("key: " + key + " value: " + value)
 
         let parametersting = ""
 
         if (validate.isArray(value)) {
-            console.log("Array: " + key + "[" + value.length + "]")
 
             value.forEach(element => {
                 parametersting += this._encode(key, element)
             });
         } else if (validate.isObject(value)) {
-            console.log("Object: " + key)
 
             for (let key in value) {
                 parametersting += this._encode(key, value[key])
             }
 
         } else {
-            console.log("Normal: " + key)
             parametersting = "&" + key + "=" + value
         }
         return parametersting
